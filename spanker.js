f907b4c6202d42dafd0367d8c0d5dce1e295b4bd
Spanker <73857014+Spxnkerr@users.noreply.github.com>
2021-08-21 15:41:43 +0300

    Delete spanker.js

--- spanker.js
+++
@@ -1,5 +0,0 @@
-Selam Dostlar, Github Hesabım Kapandı. Nedeni Bir Arkadaş Projeyi Önce Almak İçin Fake Hesap Açıp Starlıyormuş. Lütfen Böyle Bir Şey Yapmayınız.
-
-70 Star Olduğu An Projeyi Size Sunacağım.
-
-Bu Projeden Haberdar Olmak İçin Beni Github Hesabımdan Takip Edebilirsin.
